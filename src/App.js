4cce0b32b54a52d6f7f1b05aa0673fca7cbce850
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-08 21:20:57 +0530

    ADD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const styles = {
   fontFamily: "ubuntu"
